93989f79a18ef30e3847cbf00948a0437fede43e
OscarNoe <41075783+OscarNoe@users.noreply.github.com>
2019-06-05 21:21:50 -0500

    Agregacion de Bootstrap
    Agregue el framework como cdn

--- chat.php
+++ chat.php
@@ -16,13 +16,14 @@ $mostrar = mysqli_fetch_array($result);
     <title>Administración - Hora Valenciana</title>
     <link rel="icon" href="<?php echo ($favicon) ?>" sizes="32x32">
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/main.css" />
     
 </head>
 
 <style>
         body {
-            background-image: url('img/1.gif');
+            background-image: url('img/.gif');
             background-size: cover;
             background-repeat: none;
         }
@@ -45,6 +46,7 @@ $mostrar = mysqli_fetch_array($result);
     </style>
 
 <body>
+
     <center>
         <h3 class="major">Live chat</h3>
     </center>
@@ -61,8 +63,11 @@ $mostrar = mysqli_fetch_array($result);
             <div id="recargar"> </div>
         </div>
     </div>
-</body>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
 
 <script type="text/javascript">
