ac9b3d8e2bc45f41fc9d7d7fd41161b788d0a2d8
Carlos Ayala <49286868+SrMai@users.noreply.github.com>
2019-06-05 23:59:01 -0500

    Reparaciòn de chat.php
    Se reparo el chat.php  añadiendo scripts y la base de datos

--- chat.php
+++ chat.php
@@ -1,4 +1,9 @@
 <?php
+require 'database.php';
+?>
+
+
+<?php
 $message = '';
 if (!empty($_POST['nombre']) && !empty($_POST['mensaje'])) {
     $sql = "INSERT INTO live_chat (nombre, mensaje) VALUES (:nombre, :mensaje)";
@@ -13,21 +18,37 @@ if (!empty($_POST['nombre']) && !empty($_POST['mensaje'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
+<!--
+    Creditos:
+    Html5 up || @ajlkn ||
+    www.carlosayala.me
+-->
+<!DOCTYPE HTML>
+
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chat - Hora Valenciana</title>
-    <link rel="icon" href="<?php echo ($favicon) ?>" sizes="32x32">
-    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    <title>Radio-Ceti</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="assets/css/main.css" />
-    
-</head>
 
-<style>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("textarea[maxlength]").keyup(function() {
+                var limit = $(this).attr("maxlength"); // Límite del textarea
+                var value = $(this).val(); // Valor actual del textarea
+                var current = value.length; // Número de caracteres actual
+                if (limit < current) { // Más del límite de caracteres?
+                    // Establece el valor del textarea al límite
+                    $(this).val(value.substring(0, limit));
+                }
+            });
+
+        });
+    </script>
+
+    <style>
         body {
             background-image: url('img/');
             background-size: cover;
@@ -49,55 +70,122 @@ if (!empty($_POST['nombre']) && !empty($_POST['mensaje'])) {
             width: 90%;
             height: 400px;
         }
-
-        div.sticky{
-            position: -webkit-sticky;
-            position: sticky;
-            top: 0;
-            padding: 5px;
-            background-color: #000000;
-            border: 2px solid #1B2A95;
-            color: white;
-
-        }
     </style>
 
-<body>
+</head>
 
-    <center>
-        <h2 class="major">Expo-Ciencia</h2>
-    </center>
-        <div class="sticky">
-        <form method="POST" action="chat.php">
-            <div class="fields">
-                <div class="field half">
-                    <input name="nombre" type="text" placeholder="Nombre" required>
-                </div>
-                <div class="field">
-                    <input name="mensaje" type="text" placeholder="Texto" autocomplete="none" required>
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Header -->
+        <header id="header">
+            <div class="content">
+                <div class="inner">
+                    <h1>Radio-Ceti</h1>
+                    <p>Este es un proyecto escolar para la exposición de proyectos en el plantel Rio Santiago</p>
                 </div>
             </div>
-            <ul class="actions">
-                <li><input type="submit" value="Enviar" class="primary" /></li>
-            </ul>
-        </form>
-    </div>
-    <div style="color:white; border-radius:15px; margin:1%;" class="text-center bg-min-dark font-weight-light">
-        <div id="bienvenido" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-            <hr style="width: 90%;" class="hr-light">
-            <div class="card-body">
-                <div id="recargar"> </div></p>
+        </header>
+        <!-- Header -->
+        <header id="header">
+            <div class="content">
+                <nav>
+                    <ul>
+                        <li><a href="#pedir">Pedir cancion</a></li>
+                        <li><a href="#chat">Live-Chat</a></li>
+                        <li><a href="#contacto">Contacto</a></li>
+                    </ul>
+                </nav>
             </div>
+        </header>
+
+        <!-- Main -->
+        <div id="main">
+
+            <!-- Pedir una cancion -->
+            <article id="pedir">
+                <h2 class="major">¿Qué canción quieres?</h2>
+                <form method="POST" action="s_p.php">
+                    <div class="fields">
+                        <div class="field half">
+                            <label for="artista">Nombre de la canción</label>
+                            <input name="nombre" type="text" placeholder="Nombre de la canción" required>
+                        </div>
+                        <div class="field half">
+                            <label for="artista">Artista</label>
+                            <input name="artista" type="text" placeholder="¿Cual es el artista que canta esa canción?" required>
+                        </div>
+                        <div class="field">
+                            <label for="comentario">Mensaje(opcional)</label>
+                            <input name="comentario" type="text" placeholder="¿Quieres mandar algun saludo?" autocomplete="nope" required>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Pedir canción" class="primary" /></li>
+                    </ul>
+                </form>
+            </article>
+
+            <!-- Live-Chat -->
+            <article id="chat">
+                <h2 class="major">Live</h2>
+                <div id="recargar"> </div>
+                <br />
+                <form method="POST" action="index.php">
+                    <div class="fields">
+                        <div class="field half">
+                            <input name="nombre" type="text" placeholder="Nombre" required>
+                        </div>
+                        <div class="field">
+                            <textarea maxlength="250" name="mensaje" type="text" placeholder="Texto" autocomplete="none" required></textarea>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Enviar" class="primary" /></li>
+                    </ul>
+                </form>
+            </article>
+
+            <!-- Contacto -->
+            <article id="contacto">
+                <h2 class="major">Contacto</h2>
+                <form method="POST" action="s_c.php">
+                    <div class="fields">
+                        <div class="field half">
+                            <label for="nombre">Nombre</label>
+                            <input name="nombre" type="text" placeholder="Escribe tú nombre" required>
+                        </div>
+                        <div class="field half">
+                            <label for="email">Email</label>
+                            <input name="email" type="email" placeholder="contacto@tecnologos-lab.com" required>
+                        </div>
+                        <div class="field">
+                            <label for="mensaje">Mensaje</label>
+                            <textarea name="mensaje" id="message" rows="4" placeholder="Escribe redacta tu mensaje (max 400 caracteres)" required></textarea>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Enviar" class="primary" /></li>
+                    </ul>
+                </form>
+            </article>
         </div>
-    </div>
-    
-    <div class="container">
-        <div class="card-body">
-            <div id="recargar"> </div>
-        </div>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <p class="copyright">Copyright &copy; 2018-2020 <a style="text-decoration: none;" href="#">Radio-CETI</a> - Todos los derechos reservados<br></p>
+            <p class="copyright"><a href="http://www.carlosayala.me">Carlos Ayala </a> y <a href="http://www.oscarnoe.me">Oscar noe</a></p>
+        </footer>
     </div>
 
-    
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
 </body>
 
 </html>
@@ -107,7 +195,7 @@ if (!empty($_POST['nombre']) && !empty($_POST['mensaje'])) {
         setInterval(
             function() {
                 $('#recargar').load('scrips/com_live.php');
-            }, 20
+            }, 1000
         );
     });
-</script> 
+</script>
